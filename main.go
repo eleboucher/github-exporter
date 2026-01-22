051244e3c288da790b19c5f5e3e0d99a32228b87
Erwan Leboucher <erwan.leboucher@blablacar.com>
2026-01-22 22:08:54 +0100

    Handle log level better

--- main.go
+++ main.go
@@ -7,9 +7,21 @@ import (
 	"github.com/eleboucher/github-exporter/cmd"
 )
 
+func getDefaultLogLevel() slog.Level {
+	levelStr := os.Getenv("LOG_LEVEL")
+	if levelStr == "" {
+		return slog.LevelInfo
+	}
+	var level slog.Level
+	if err := level.UnmarshalText([]byte(levelStr)); err != nil {
+		return slog.LevelInfo
+	}
+	return level
+}
+
 func main() {
 	opts := &slog.HandlerOptions{
-		Level: slog.LevelDebug,
+		Level: getDefaultLogLevel(),
 	}
 
 	handler := slog.NewJSONHandler(os.Stdout, opts)
