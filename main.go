e73a564ab0760c6290e96a597cd23d40cf1ec735
Erwan Leboucher <erwan.leboucher@blablacar.com>
2026-01-22 19:34:38 +0100

    add debug

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/eleboucher/github-exporter/cmd"
+import (
+	"log/slog"
+	"os"
+
+	"github.com/eleboucher/github-exporter/cmd"
+)
 
 func main() {
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}
+
+	handler := slog.NewJSONHandler(os.Stdout, opts)
+	logger := slog.New(handler)
+
+	slog.SetDefault(logger)
 	cmd.Execute()
 }
